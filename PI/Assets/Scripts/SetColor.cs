a95e3592f6b28bd453023f115a030e7eea61db77
Paolo <s265525@studenti.polito.it>
2019-11-22 12:26:27 +0100

    Script Example

---
+++ PI/Assets/Scripts/SetColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetColor : MonoBehaviour
+{
+
+    [SerializeField]
+    private Color colorToSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseOver()
+    {
+        transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = colorToSet;
+    }
+}
