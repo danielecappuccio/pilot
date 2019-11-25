2d39f9ec0138b85f7fdfa94203e0aa6832f35d1a
Paolo <s265525@studenti.polito.it>
2019-11-26 00:36:11 +0100

    Inserted "Home" button

---
+++ PI/Assets/Scripts/ChangeScreen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScreen : MonoBehaviour
+{
+    public int scene;
+
+    public void ChangeScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
