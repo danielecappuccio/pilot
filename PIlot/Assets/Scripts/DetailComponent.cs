1db9068477104720b24078a258750646d6843065
Paolo <s265525@studenti.polito.it>
2019-12-05 16:57:31 +0100

    Added temporary assembly camera

---
+++ PIlot/Assets/Scripts/DetailComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailComponent : MonoBehaviour
+{
+
+    private Vector3 mouseReference;
+    private bool isDragging = false;
+    private float sensitivityFactor = 0.01f;
+
+    [SerializeField]
+    private int dragSensitivity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnMouseExit()
+    {
+        isDragging = false;
+    }
+
+    private void OnMouseDown()
+    {
+        mouseReference = Input.mousePosition;
+        isDragging = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 offset = Input.mousePosition - mouseReference;
+        gameObject.transform.Rotate(sensitivityFactor * dragSensitivity * offset.y, -sensitivityFactor * dragSensitivity * offset.x, 0, Space.World);
+        mouseReference = Input.mousePosition;
+    }
+}
---
+++ PIlot/Assets/Scripts/TMPCam.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TMPCam : MonoBehaviour
+{
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
