781bb9f95ec87cc98493d2b837c91e5cad504bc5
Paolo <s265525@studenti.polito.it>
2019-12-08 16:08:37 +0100

    Many things...

---
+++ PIlot/Assets/Scripts/TaskImage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskImage : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+
+    private bool isOpen = false;
+
+    public void TaskSelect()
+    {
+        if (isOpen)
+        {
+            transform.Translate(new Vector3(camera.pixelWidth, 0, 0));
+            camera.gameObject.SetActive(false);
+            this.isOpen = false;
+        }
+        else
+        {
+            transform.Translate(new Vector3(-camera.pixelWidth, 0, 0));
+            camera.gameObject.SetActive(true);
+            this.isOpen = true;
+        }
+    }
+}
