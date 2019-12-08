ec3969f1930d59fb8bf4779381cac624c48d97ae
Paolo <s265525@studenti.polito.it>
2019-12-08 17:00:28 +0100

    Other stuff

---
+++ PIlot/Assets/Scripts/WarningLogo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarningLogo : MonoBehaviour
+{
+    public RawImage image;
+
+    public void ShowWarning()
+    {
+        image.gameObject.SetActive(true);
+    }
+
+    public void RemoveWarning()
+    {
+        image.gameObject.SetActive(false);
+    }
+}
